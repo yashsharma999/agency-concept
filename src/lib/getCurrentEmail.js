09c66bf44ed3eb308ab51524e2f4e679c71a5293
Yash Sharma <syash5575@gmail.com>
2024-06-03 23:35:04 +0530

    chore: debug mutation issue

--- src/lib/getCurrentEmail.js
+++ src/lib/getCurrentEmail.js
@@ -2,10 +2,13 @@ import { clerkClient, currentUser } from '@clerk/nextjs';
 
 export default async function getEmailAddress() {
   const user = await currentUser();
+
   if (user) {
     const response = await clerkClient?.emailAddresses?.getEmailAddress(
       user?.primaryEmailAddressId
     );
+
+    console.log('response', response);
 
     return response?.emailAddress;
   }
